75107adcd4959a3ec28137293dfd025e64a88ec9
CSE-D_section <nnm22cs100@nmamit.in>
2024-11-23 16:03:27 +0530

    added CV

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -37,7 +37,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Full Stack Software Engineer at Blackfrog Technologies
+            A Full Stack Software Engineer at Blackfrog Technologies
           </p>
           <div>
             <Link
@@ -47,8 +47,9 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="/file/Tasmiya_CV.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+              download
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
